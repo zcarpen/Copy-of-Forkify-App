aba02b3c479124fdef55a37c862d1ae539504987
zachCarpenter <zcarpen@gmail.com>
2021-12-08 17:37:44 +0400

    Copy commit.

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'feda1890-a54c-489b-be88-6a63fde826a7';
+export const MODAL_CLOSE_SEC = 2.5;
